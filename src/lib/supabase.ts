be8356c0a267e4d5f8b8546f74be7677d865f143
Aarab <aarabm384@gmail.com>
2026-04-02 03:50:31 +0200

    fix: static supabase connection

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-)
+// هاد القيم دابا محطوطين ديريكت باش نقطعو الشك فـ الـ Connection
+const supabaseUrl = 'https://koofjrlzfelfmkbgzfpn.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtvb2Zqcmx6ZmVsZm1rYmd6ZnBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA2OTIyMTYsImV4cCI6MjA1NjI2ODIxNn0.V_XkGkm1he8D3_SsqIszvug_lSBGT0Jd78yR5N_rdxl'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
