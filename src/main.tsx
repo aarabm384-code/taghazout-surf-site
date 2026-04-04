56eb70845a0f70c55096db748062bf3969dfb01d
Aarab <aarabm384@gmail.com>
2026-04-04 23:20:27 +0200

    enable vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+// 1. استيراد المكون ديال التحليلات
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    {/* 2. إضافة المكون باش يبدا يحسب الزوار */}
+    <Analytics />
   </StrictMode>
 );
